c39efcbcf1f63e7a52fecd81dee021442b3f35a8
Ryan Trickett <Ryan.Trickett@standardbank.co.za>
2024-03-05 09:38:39 +0200

    🎉 (project) Project Setup

---
+++ apps/api/src/test/java/com/aricloud/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aricloud.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
