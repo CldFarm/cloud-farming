36b158bce686c7d7321cab61de17524b225505b1
ThapeloThoka <156646690+ThapeloThoka@users.noreply.github.com>
2024-03-06 15:13:36 +0200

    basic cli and api endpoint

--- apps/api/src/main/java/com/aricloud/api/ApiApplication.java
+++ apps/api/src/main/java/com/aricloud/api/ApiApplication.java
@@ -2,12 +2,24 @@ package com.aricloud.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+	@RestController
+	public static class MathController {
+
+		@GetMapping("/add")
+		public int addNumbers(@RequestParam int num1, @RequestParam int num2) {
+			return num1 + num2;
+		}
 	}
 
 }
