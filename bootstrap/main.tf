b8d3168039fc9c986d07a04fb7c1c3b216d54976
Karl <karl.etsebeth@bbd.co.za>
2024-03-06 10:48:40 +0200

    Updated bootstrap

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,5 +1,11 @@
 provider "aws" {
   region = "eu-west-1"
+  default_tags {
+    tags = {
+      "owner"         = "karl.etsebeth@bbd.co.za"
+      "created-using" = "terraform"
+    }
+  }
 }
 
 resource "aws_s3_bucket" "terraform_state" {
@@ -25,4 +31,44 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
     name = "LockID"
     type = "S"
   }
+}
+
+resource "aws_iam_openid_connect_provider" "githubActions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+resource "aws_iam_role" "githubActions" {
+  name                 = "GithubRunner"
+  assume_role_policy   = local.trust_policy
+  managed_policy_arns  = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+  max_session_duration = 3600
+}
+
+output "githubActionsRole" {
+  value = aws_iam_role.githubActions.arn
+}
+
+locals {
+  trust_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : {
+            "Federated" : "arn:aws:iam::387198229710:oidc-provider/token.actions.githubusercontent.com"
+          },
+          "Action" : "sts:AssumeRoleWithWebIdentity",
+          "Condition" : {
+            "StringEquals" : {
+              "token.actions.githubusercontent.com:sub" : "repo:CrafDb-Team/crafdb/*",
+              "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+            }
+          }
+        }
+      ]
+    }
+  )
 }
