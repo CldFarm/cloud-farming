11f5174cf0557200994088e24b36410bbb0fc924
Ryan Trickett <Ryan.Trickett@standardbank.co.za>
2024-03-07 14:47:22 +0200

    Fixed Cli Issue

--- apps/cli/src/main/java/com/agricloud/cli/CliApplication.java
+++ apps/cli/src/main/java/com/agricloud/cli/CliApplication.java
@@ -1,9 +1,11 @@
-package com.aricloud.cli;
+package com.agricloud.cli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.agricloud")
 public class CliApplication {
 
 	public static void main(String[] args) {
