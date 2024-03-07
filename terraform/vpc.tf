513e94efea2247d30778e9a1cada7679888ac74e
Karl <karl.etsebeth@bbd.co.za>
2024-03-07 19:39:24 +0200

    Add vpc

---
+++ terraform/vpc.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "main-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway                     = true
+  enable_vpn_gateway                     = true
+  create_igw                             = true
+  create_database_subnet_group           = true
+  create_database_subnet_route_table     = true
+  create_database_internet_gateway_route = true
+  enable_dns_hostnames                   = true
+  enable_dns_support                     = true
+}
