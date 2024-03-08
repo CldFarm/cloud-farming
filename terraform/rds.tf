e8600e6d1857f62e2bc453aec38a43e0c0cda0a2
Ryan Trickett <Ryan.Trickett@standardbank.co.za>
2024-03-08 12:01:03 +0200

    Fixed Terraform Error

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -15,7 +15,7 @@ module "rds" {
   deletion_protection = false
   skip_final_snapshot = true
 
-  db_subnet_group_name = module.vpc.database_subnet_group_name
+  db_subnet_group_name   = module.vpc.database_subnet_group_name
   vpc_security_group_ids = [aws_security_group.rds.id]
   publicly_accessible    = true
 
