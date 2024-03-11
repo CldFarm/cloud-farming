051fd21e05994212bd0043b291802652a33fc84e
Sean van Wyk <sean.vanwyk@bbd.co.za>
2024-03-11 18:31:49 +0200

    Fix Autowiring not working (And add RestController)

--- apps/api/src/main/java/com/agricloud/ApiApplication.java
+++ apps/api/src/main/java/com/agricloud/ApiApplication.java
@@ -1,12 +1,10 @@
-package com.agricloud.api;
+package com.agricloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan("com.agricloud")
 @EnableJpaRepositories
 public class ApiApplication {
 
